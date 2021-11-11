d124c9fe00aa63626efa14c773a34ce728f92036
Saaransh <saaranshsharma28@gmail.com>
2021-11-11 00:09:16 -0500

    setting up

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from logging import debug
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../Bookstore/bookstoreDB.db'
+app.config['SECRET_KEY'] = 'password'
 
 @app.route('/')
 def index():
@@ -10,6 +18,13 @@ def index():
 @app.route('/login', methods=["POST", "GET"])
 def login():
     return render_template("login.html")
+
+
+@app.route('/signup', methods=["POST", "GET"])
+def signup():
+    return render_template("signup.html")
+
+
 
 
 if __name__ == "__main__":
