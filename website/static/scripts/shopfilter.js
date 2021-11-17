de8a249dc8bcedd4b29328fc962acc21f1dfa69d
Saaransh <saaranshsharma28@gmail.com>
2021-11-16 22:08:56 -0500

    update

--- website/static/scripts/shopfilter.js
+++ website/static/scripts/shopfilter.js
@@ -1,45 +1,64 @@
 const className = document.getElementsByClassName("scontainer");
-
-var fantasy =  document.getElementById("card");
-console.log(fantasy)
-//var fiction =  className[0].getElementsByClassName("Fiction");
-//var nonfiction =  className[0].getElementsByClassName("Non-Fiction");
-//var action =  className[0].getElementsByClassName("Action");
-//var adventure =  className[0].getElementsByClassName("Adventure");
-//var comics =  className[0].getElementsByClassName("Comics");
-
-//Handles button selection
-/*
 function shopFilter(selection){
     
     if(selection==='all'){
         visible();
                 
-    }else{
+    }
+    else{
         cardSelect(selection);
     }
 }
-
-//Sets visibility for respective categories
 function visible(){
     className[0].style.visibility = "visible";
 
+    var action =  className[0].getElementsByClassName("Action");
+    var adventure =  className[0].getElementsByClassName("Adventure");
+    var comics =  className[0].getElementsByClassName("Comics");
+    var fantasy =  className[0].getElementsByClassName("Fantasy");
+    var fiction =  className[0].getElementsByClassName("Fiction");
+    var nonfiction =  className[0].getElementsByClassName("Non-Fiction");
+
+    for(let i = 0;i<action.length;i++){
+        action[i].style.display="grid";
+               
+    }
+    for(let i = 0;i<adventure.length;i++){
+        adventure[i].style.display="grid";
+               
+    }
+    for(let i = 0;i<comics.length;i++){
+        comics[i].style.display="grid";
+               
+    }
     for(let i = 0;i<fantasy.length;i++){
         fantasy[i].style.display="grid";
                
     }
     for(let i = 0;i<fiction.length;i++){
         fiction[i].style.display="grid";
+               
+    }
+    for(let i = 0;i<nonfiction.length;i++){
+        nonfiction[i].style.display="grid";
         
     }
     
 }
 
-//Handles category selection, when a category is selected, visibility of others set to none
-function cardSelect(genre){
- 
+function cardSelect(card){
+
+    var action =  className[0].getElementsByClassName("Action");
+    var adventure =  className[0].getElementsByClassName("Adventure");
+    var comics =  className[0].getElementsByClassName("Comics");
+    var fantasy =  className[0].getElementsByClassName("Fantasy");
+    var fiction =  className[0].getElementsByClassName("Fiction");
+    var nonfiction =  className[0].getElementsByClassName("Non-Fiction");
+
+    
     visible();
-    if(genre==='Fantasy'){
+
+    if(card==='Fantasy'){
         
         for(let i = 0;i<fiction.length;i++){
             fiction[i].style.display="none";
@@ -53,4 +72,3 @@ function cardSelect(genre){
     } 
         
 }
-*/
