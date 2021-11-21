94606370ecbb1dc4005fe8142f7194733433cdcf
Mfrack22 <74574227+Mfrack22@users.noreply.github.com>
2021-11-20 20:17:46 -0500

    register fix

--- website/views.py
+++ website/views.py
@@ -8,7 +8,7 @@ import json
 
 views = Blueprint('views',__name__)
 
-@views.route('/')
+@views.route('/',methods=['GET', 'POST'])
 
 def home():
     booksJOINgenre =  db.session.query(Book,Genre).select_from(Book).join(Genre).all()
